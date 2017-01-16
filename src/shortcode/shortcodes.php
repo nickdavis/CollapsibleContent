39f9bd3560b89087e971503a823979c9510c6692
Nick Davis <nick@esanctuary.ch>
2017-01-16 15:55:07 +0100

    built the HTML view files

--- src/shortcode/shortcodes.php
+++ src/shortcode/shortcodes.php
@@ -8,3 +8,7 @@
  * @link        https://iamnickdavis.com
  * @license     GNU General Public License 2.0+
  */
+
+// [qa question="What question would you like to ask me?" show_icon="" hide_icon=""]this is the answer[/qa]
+
+// [teaser visible_message="What question would you like to ask me?" show_icon="" hide_icon=""]this is the answer[/teaser]
